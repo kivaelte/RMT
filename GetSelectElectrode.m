a133e85e2b251e12c172f4242e3721682ed881aa
kivaelte <104213759+kivaelte@users.noreply.github.com>
2023-05-22 14:51:11 +0200

    GetSelectElectrode.m

--- GetSelectElectrode.m
+++ GetSelectElectrode.m
@@ -1,24 +1,44 @@
-function SE = GetSelectElectrode(EEG,Spectrum)  
+function SE = GetSelectElectrode(EEG, Spectrum)
 
-%Select the relevant electrodes
+% Select the relevant electrodes
 %
 % Usage: SE = GetSelectElectrode(EEG, Spectrum)
 %
-% Input(s): 
+% Input(s):
 % EEG = EEG struct
-% Spectrum = range of frequency to inspect, e.g. [3 8] for theta
+% Spectrum = range of frequency to inspect, e.g., [8 12] for alpha
 %
 % Output:
 % SE = selected electrode
-% 
+%
 % Author: K.V.E. Temme
 %
-% thoughts for later: slice EEG dataset for relevant electrodes
+% Thoughts for later: slice EEG dataset for relevant electrodes
 
-for i=1:64
-    relpower(i) = 100*(bandpower(EEG.data(i,:),100,Spectrum)/ ...
-        bandpower(EEG.data(i,:),100,[0 44]));
+numElectrodes = 64;
+alphaPower = zeros(1, numElectrodes);
+
+for i = 1:numElectrodes
+    alphaPower(i) = 100 * (bandpower(EEG.data(i,:), EEG.srate, Spectrum) / ...
+        bandpower(EEG.data(i,:), EEG.srate, [0 44]));
 end
 
+% Plot the top view of the head with electrode locations
+figure;
+topoplot(alphaPower, EEG.chanlocs, 'maplimits', [min(alphaPower), max(alphaPower)], 'colormap', jet);
 
-[maxrelpower,SE] = max(relpower)
+% Set the color bar and title
+colorbar;
+title('Alpha Power Distribution on the Head');
+
+% Select the electrode with the highest alpha power
+[~, maxAlphaElectrode] = max(alphaPower);
+selectedElectrode = EEG.chanlocs(maxAlphaElectrode).labels;
+
+% Display the selected electrode
+text(EEG.chanlocs(maxAlphaElectrode).X, EEG.chanlocs(maxAlphaElectrode).Y, 'X', 'Color', 'red');
+text(EEG.chanlocs(maxAlphaElectrode).X, EEG.chanlocs(maxAlphaElectrode).Y, selectedElectrode, 'Color', 'red');
+
+SE = selectedElectrode;
+
+end
